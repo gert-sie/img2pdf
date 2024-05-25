67d2ab385b5f5c8693150155eeb9bbeddc87c895
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-05-25 14:54:01 +0200

    Update img2pdf.py Pfade angepasst

--- img2pdf.py
+++ img2pdf.py
@@ -1,11 +1,11 @@
 from PIL import Image  # install by > python3 -m pip install --upgrade Pillow  # ref. https://pillow.readthedocs.io/en/latest/installation.html#basic-installation
 
 images = [
-    Image.open("/Users/apple/Desktop/" + f)
-    for f in ["bbd.jpg", "bbd1.jpg", "bbd2.jpg"]
+    Image.open("/home/Bilder" + f)
+    for f in ["20240523_011830.jpg", "20240523_011913.jpg", "20240523_012402.jpg"]
 ]
 
-pdf_path = "/Users/apple/Desktop/bbd1.pdf"
+pdf_path = "/home/Bilder/bbd1.pdf"
     
 images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
