7dd70f66ebb3e26980da8c1bc8c8eb6b25eb1584
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-05-31 17:23:18 +0200

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -16,9 +16,8 @@ pdf_path = "/home/gert/Bilder/bilder-test.pdf"
 images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
 )
-#canvas = Canvas(window,width = 600,height = 400,bg = '#6699ff')
-canvas=Canvas(expand=YES, fill=BOTH)
-canvas.pack()
+canvas = Canvas(window,width = 600,height = 400,bg = '#6699ff')
+canvas.pack(expand=YES, fill=BOTH)
 python_image = tk.PhotoImage(f'/home/gert/Bilder/20230821_112419.jpg')
 image_item = canvas.create_image(
     (100, 100),
