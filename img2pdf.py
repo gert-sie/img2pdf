a2a6c4d72dc6593832fbf63d9ef30778f9a91bc7
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-05-31 17:48:19 +0200

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -17,13 +17,7 @@ images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
 )
 canvas = Canvas(window,width = 600,height = 400,bg = 'white')
-canvas.pack()
-python_image = tk.PhotoImage(f'/home/gert/Bilder/pinwheel-8829_128.gif')
-image_item = canvas.create_image(
-    (10, 10),
-    image=python_image
-)
-canvas = tk.Canvas(root, width=600, height=400, bg='white')
+#canvas.pack()
 canvas.pack(anchor=tk.CENTER, expand=True)
 
 python_image = tk.PhotoImage(file='/home/gert/Bilder/pinwheel-8829_128.gif')
