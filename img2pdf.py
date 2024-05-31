291c5a282ac4515869870e92e76318cadc97c349
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-05-31 16:06:56 +0200

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -18,7 +18,7 @@ images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
 )
 canvas.pack()
-python_image = Image.open(file='/home/gert/Bilder/20230821_112419.jpg')
+python_image = Image.open('/home/gert/Bilder/20230821_112419.jpg')
 canvas.create_image(
     (100, 100),
     image=python_image
