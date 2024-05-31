a0e79753cf4dc444a4fe89f48f12e68df30691bc
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-05-31 17:09:20 +0200

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -17,13 +17,10 @@ pdf_path = "/home/gert/Bilder/bilder-test.pdf"
 images[0].save(
     pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
 )
-canvas.pack()
 python_image = tk.PhotoImage(f'/home/gert/Bilder/20230821_112419.jpg')
 image_item = canvas.create_image(
     (100, 100),
     image=python_image
 )
-label = Label(image=image_item) 
-label.image = image_item # keep a reference!
-label.pack()
+canvas.pack()
 window.mainloop()
