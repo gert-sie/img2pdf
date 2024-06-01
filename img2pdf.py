0db6d711a9171b7e4e3acf61d4c0b90dc8d13b06
gert-sie <155890033+gert-sie@users.noreply.github.com>
2024-06-01 15:41:15 +0200

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -26,6 +26,9 @@ canvas.create_image(
     (100, 100),
     image=python_image
 )
+Überschrift = "Label1"
+l1 = Label (window,text=Überschrift)
+l1.place(x = 0, y = 0, width=120, height=15)
 
 '''
 # Load the image file
